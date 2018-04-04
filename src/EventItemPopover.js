2904a8b6979e8e43e3be5b3553c850a862ddea8b
Jindrich Svab <jindrich.svab@gmail.com>
2018-04-04 14:21:22 +0200

    migration to react 16

--- src/EventItemPopover.js
+++ src/EventItemPopover.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import {PropTypes} from 'prop-types'
 import {Row, Col} from 'antd'
 
 class EventItemPopover extends Component {
