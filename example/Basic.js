dd57113d5bffd389c552c2f911837cb1015e122f
Jindrich Svab <jindrich.svab@gmail.com>
2018-04-05 09:41:57 +0200

    examples migration to react 16

--- example/Basic.js
+++ example/Basic.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import {PropTypes} from 'prop-types' 
 //import moment from 'moment'
 //import 'moment/locale/zh-cn';
 import {DragDropContext} from 'react-dnd'
