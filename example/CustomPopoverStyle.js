d7cc098765e1696cb529c5157109bfcb69ec8721
Stephen Chou <27788280@qq.com>
2019-02-15 00:27:05 +0800

    Update online demo

--- example/CustomPopoverStyle.js
+++ example/CustomPopoverStyle.js
@@ -5,7 +5,7 @@ import {PropTypes} from 'prop-types'
 import Col from 'antd/lib/col'
 import Row from 'antd/lib/row'
 import Button from 'antd/lib/button'
-import 'antd/lib/style/index.less';     //Add this code for locally example
+// import 'antd/lib/style/index.less';     //Add this code for locally example
 import Scheduler, {SchedulerData, ViewTypes, DATE_FORMAT, DemoData} from '../src/index'
 import Nav from './Nav'
 import ViewSrcCode from './ViewSrcCode'
