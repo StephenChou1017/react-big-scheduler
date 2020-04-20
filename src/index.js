7ff335dcf6059fa8b9573a8dfa7559404780216f
Karl Fusaris <karl@paceup.com>
2020-04-20 16:50:01 +0100

    Fix DatePicker reference

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ import Radio from "antd/lib/radio";
 import "antd/lib/radio/style/index.css";
 import Popover from "antd/lib/popover";
 import "antd/lib/popover/style/index.css";
-import DatePicker from "antd/lib/datepicker";
+import { DatePicker } from 'antd'
 import "antd/lib/calendar/style/index.css";
 import EventItem from "./EventItem";
 import DnDSource from "./DnDSource";
