6dc9107a6171f5b176977d96016ce38aa65c2068
Karl Fusaris <karl@paceup.com>
2020-05-12 07:57:34 +0100

    Add type definitions

--- scripts/build.js
+++ scripts/build.js
@@ -1,73 +1,79 @@
-'use strict';
+"use strict";
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", (err) => {
   throw err;
 });
 
-const util = require('util');
-const path = require('path');
-const exec = util.promisify(require('child_process').exec);
-const appendFile = util.promisify(require('fs').appendFile);
+const util = require("util");
+const path = require("path");
+const exec = util.promisify(require("child_process").exec);
+const appendFile = util.promisify(require("fs").appendFile);
 
 async function build() {
-  const root = path.resolve(__dirname, '..');
-  const sourceDir = path.resolve(root, 'src');
-  const targetDir = path.resolve(root, 'lib');
+  const root = path.resolve(__dirname, "..");
+  const sourceDir = path.resolve(root, "src");
+  const targetDir = path.resolve(root, "lib");
   const jsTarget = targetDir;
-  const cssTarget = path.resolve(targetDir, 'css');
-  const hackFileName = 'antd-globals-hiding-hack'
+  const cssTarget = path.resolve(targetDir, "css");
+  const hackFileName = "antd-globals-hiding-hack";
   const hackFileSource = path.resolve(
     sourceDir,
-    'less',
-    hackFileName + '.less'
+    "less",
+    hackFileName + ".less"
   );
-  const hackFileOutputPath = path.resolve(
-    cssTarget,
-    hackFileName + '.css'
-  );
+  const hackFileOutputPath = path.resolve(cssTarget, hackFileName + ".css");
 
   try {
     // clean
-    process.stdout.write('Cleaning... \n');
-    const cleanResult = await exec('npm run clean');
+    process.stdout.write("Cleaning... \n");
+    const cleanResult = await exec("npm run clean");
 
     // transpiling and copy js
-    process.stdout.write('Transpiling js with babel... \n');
+    process.stdout.write("Transpiling js with babel... \n");
     const jsResult = await exec(`babel ${sourceDir} --out-dir ${jsTarget}`);
 
     // copy css
-    process.stdout.write(`Copying library style definitions to ${cssTarget}/style.css ... \n`);
-    const cssResult = await exec(`mkdir -p ${cssTarget} && cp ${sourceDir}/css/style.css ${cssTarget}/style.css`);
+    process.stdout.write(
+      `Copying library style definitions to ${cssTarget}/style.css ... \n`
+    );
+    const cssResult = await exec(
+      `mkdir -p ${cssTarget} && cp ${sourceDir}/css/style.css ${cssTarget}/style.css`
+    );
+
+    // copy types
+    process.stdout.write(
+      `Copying type definitions to ${jsTarget}/index.d.ts ... \n`
+    );
+    const tsResult = await exec(
+      `cp ${sourceDir}/index.d.ts ${jsTarget}/index.d.ts`
+    );
 
     // compile antd-hack less into css and copy it into lib
-    process.stdout.write('Implementing antd hack... \n');
-    const heckResult = await exec(
+    process.stdout.write("Implementing antd hack... \n");
+    const hackResult = await exec(
       `lessc --js ${hackFileSource} ${hackFileOutputPath}`
     );
     // append lib/index.js with line importing antd-hack
     const linesToBeAdded = [
-      '',
-      '',
-      '// this line has been added by scripts/build.js',
+      "",
+      "",
+      "// this line has been added by scripts/build.js",
       "require('./css/antd-globals-hiding-hack.css');",
-      '',
-    ]
-    await appendFile(
-      `${targetDir}/index.js`,
-      linesToBeAdded.join('\n')
-    );
+      "",
+    ];
+    await appendFile(`${targetDir}/index.js`, linesToBeAdded.join("\n"));
 
-    process.stdout.write('Success! \n');
+    process.stdout.write("Success! \n");
   } catch (e) {
-    process.stderr.write(e)
-    process.exit()
+    process.stderr.write(e);
+    process.exit();
   }
 }
 
